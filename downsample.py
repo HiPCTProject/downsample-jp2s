8dab9eb83c5e1828da3014a0158efc7d017f67c5
David Stansby <dstansby@gmail.com>
2023-09-28 11:34:21 +0100

    Add pre-commit and tidy diffing

--- downsample.py
+++ downsample.py
@@ -6,17 +6,27 @@ import sys
 from typing import List
 
 
-def fix_old_names(datasets: List[HiPCTScan], bin_factor: int):
-    # Rename existing downsampled JP2 directories if they have the wrong name
-    differ = difflib.Differ()
+differ = difflib.Differ()
 
+
+def print_diff(a: str, b: str) -> None:
+    """
+    Print a visual diff of two strings.
+    """
+    sys.stdout.writelines(list(differ.compare([a + "\n"], [b + "\n"])))
+
+
+def fix_old_names(datasets: List[HiPCTScan], bin_factor: int) -> None:
+    # Rename existing downsampled JP2 directories if they have the wrong name
     for dataset in datasets:
         original_path = dataset.esrf_jp2_path
         # Get expeted downsample path
         downsampled_dataset = deepcopy(dataset)
         downsample_res = bin_factor * dataset.resolution_um
         downsampled_dataset.resolution_um = downsample_res
-        downsampled_path_expected = dataset.esrf_jp2_path.parent / downsampled_dataset.esrf_jp2_path.name
+        downsampled_path_expected = (
+            dataset.esrf_jp2_path.parent / downsampled_dataset.esrf_jp2_path.name
+        )
         if downsampled_path_expected.exists():
             continue
 
@@ -26,14 +36,7 @@ def fix_old_names(datasets: List[HiPCTScan], bin_factor: int):
             if downsample_path != downsampled_path_expected:
                 # Rename
                 print("Rename?")
-                sys.stdout.writelines(
-                    list(
-                        differ.compare(
-                            [str(downsample_path) + "\n"],
-                            [str(downsampled_path_expected) + "\n"]
-                        )
-                    )
-                )
+                print_diff(str(downsample_path), str(downsampled_path_expected))
                 inp = input("Continue? ")
                 if inp != "y":
                     print("skipping...")
@@ -44,8 +47,7 @@ def fix_old_names(datasets: List[HiPCTScan], bin_factor: int):
     print("Finished fixing directory names")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     datasets = load_scans()
     bin_factor = 2
 
@@ -56,8 +58,10 @@ if __name__ == '__main__':
         downsampled_dataset = deepcopy(dataset)
         downsampled_dataset.resolution_um = 2 * dataset.resolution_um
 
-        downsampled_path_expected = dataset.esrf_jp2_path.parent / downsampled_dataset.esrf_jp2_path.name
-        if not(downsampled_path_expected.exists()):
+        downsampled_path_expected = (
+            dataset.esrf_jp2_path.parent / downsampled_dataset.esrf_jp2_path.name
+        )
+        if not (downsampled_path_expected.exists()):
             print(downsampled_path_expected.parent)
             print(downsampled_path_expected)
             print()
